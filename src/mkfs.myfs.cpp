1f25f6cf6cef06df9a8987a5cfc63cd4b793a298
davidodenwald <david.odenwald91@gmail.com>
2018-12-13 13:34:47 +0100

    changed st_blocks to actual used

--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -72,27 +72,29 @@ int main(int argc, char *argv[]) {
                     filePath);
             exit(EFBIG);
         }
-        int blockCount = 0;
+
         if (file->stat.st_size % BD_BLOCK_SIZE == 0) {
-            blockCount = file->stat.st_size / BD_BLOCK_SIZE;
+            file->stat.st_blocks = file->stat.st_size / BD_BLOCK_SIZE;
         } else {
-            blockCount = file->stat.st_size / BD_BLOCK_SIZE + 1;
+            file->stat.st_blocks = file->stat.st_size / BD_BLOCK_SIZE + 1;
         }
 
-        uint16_t *blocks = (uint16_t *)malloc(sizeof(uint16_t) * blockCount);
-        dmap.getFree(blockCount, blocks);
-        if (blocks[blockCount - 1] > FILES_INDEX + FILES_SIZE) {
+        printf("%ld\n", file->stat.st_blocks);
+
+        uint16_t *blocks = (uint16_t *)malloc(sizeof(uint16_t) * file->stat.st_blocks);
+        dmap.getFree(file->stat.st_blocks, blocks);
+        if (blocks[file->stat.st_blocks - 1] > FILES_INDEX + FILES_SIZE) {
             fprintf(stderr,
                     "error: not enough space for file %s in filesystem\n",
                     filePath);
             exit(EFBIG);
         }
-        dmap.allocate(blockCount, blocks);
+        dmap.allocate(file->stat.st_blocks, blocks);
         file->firstBlock = blocks[0];
 
         // FAT
         int k;
-        for (k = 1; k < blockCount; k++) {
+        for (k = 1; k < file->stat.st_blocks; k++) {
             fat.write(blocks[k - 1], blocks[k]);
         }
         fat.write(blocks[k - 1], 0);
@@ -104,7 +106,7 @@ int main(int argc, char *argv[]) {
         // write Bytes
         std::ifstream fileStream(filePath);
         char buffer[BD_BLOCK_SIZE];
-        for (int i = 0; i < blockCount; i++) {
+        for (int i = 0; i < file->stat.st_blocks; i++) {
             // set all array items to 0
             memset(buffer, 0, sizeof(buffer));
             fileStream.read(buffer, BD_BLOCK_SIZE);
@@ -119,25 +121,6 @@ int main(int argc, char *argv[]) {
     s->fileCount = 2;
     sb.write(s);
     free(s);
-
-    // Test
-    sbStats *tmpSbStats = (sbStats*) malloc(BD_BLOCK_SIZE);
-    sb.read(tmpSbStats);
-    printf("File Count: %d\n", tmpSbStats->fileCount);
-    free(tmpSbStats);
-
-    for (int i = 0; i < rd.len(); i++) {
-        dpsFile tmpDpsFile;
-        rd.read(i, &tmpDpsFile);
-
-        uint16_t block;
-        char *buffer = (char *) malloc(BD_BLOCK_SIZE);
-        for(block = tmpDpsFile.firstBlock; block != 0; block = fat.read(block)) {
-            blockDev.read(block, buffer);
-            printf("%s", buffer);
-        }
-        free(buffer);
-    }
 
     blockDev.close();
     return 0;
