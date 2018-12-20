6994c2fcec726b6ad78105d94a3656e7c765e2bf
davidodenwald <david.odenwald91@gmail.com>
2018-12-20 17:27:27 +0100

    added read testscript

---
+++ testscripts/read.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MOUNTDIR=mountdir
+TESTFILE=Makefile
+
+./mkfs.myfs container $TESTFILE
+./mount.myfs container logfile $MOUNTDIR -s
+
+diff $TESTFILE $MOUNTDIR/$TESTFILE
+if [ $? -eq 0 ]; then
+    echo -e "\e[92mReading a file was successful.\e[39m"
+else
+    echo -e "\e[91mReading a file failed.\e[39m"
+fi
+
+sleep 0.5
+fusermount --unmount mountdir
